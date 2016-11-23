67049ce99f34f7c57de1a9d3b068cf23633e8bd8
Rahul Teja <rahulteja164@gmail.com>
2016-11-23 12:55:32 +0530

    Code Refactored to internals (#6)

--- upload.js
+++ upload.js
@@ -1,7 +1,41 @@
 var fs = require('fs');
 
-  exports.csvToJson = function(path,options){
-    //initilizing limit,delimiter,header options
+var internals = {};
+
+internals.getHeaders = function(headerData,delimiter){
+  //split the first line of data after \n
+  var headers = headerData.split('\n')[0];
+  var headerValueArr = headers.split(delimiter);
+  //return array of headers.
+  return headerValueArr;
+};
+
+internals.combineData = function(header,rawData,delimiter,headValue,finalData){
+      //store data in result object.
+      var result = {};
+      //split the data afte , and return a array.
+      var bodyData = rawData.split(delimiter);
+      // check if header value is true, if true then set limit to haeder length.
+      if(headValue){
+        limit = header.length;
+      } else {
+        //if header value is false, set limit to bodyData length
+        limit = bodyData.length;
+      }
+       for(var i=0;i < limit;i++){
+        if(headValue){
+          //maping header with body data with header option is true.
+          result[header[i]]=bodyData[i];
+        } else {
+          //maping body data if header option is false.
+            result[i] = bodyData[i];
+        }
+       }
+       //pushing result object into array.
+       finalData.push(result);
+};
+
+internals.parseData = function(path,options){
     var limit = options.limit;
     var delimiter = options.delimiter || ',';
     var headValue = options.header || true;
@@ -13,61 +47,32 @@ var fs = require('fs');
     readbleStream.on('data',function(chunk){
       data += chunk;
     });
-
     readbleStream.on('end',function(){
-      //function to get headers
-      var getHeaders = function(headerData){
-        //split the first line of data after \n
-        var headers = headerData.split('\n')[0];
-        var headerValueArr = headers.split(delimiter);
-        //return array of headers.
-        return headerValueArr;
-      };
-      var combineData = function(header,rawData){
-        //store data in result object.
-        var result = {};
-        //split the data afte , and return a array.
-        var bodyData = rawData.split(delimiter);
-        // check if header value is true, if true then set limit to haeder length.
-        if(headValue){
-          limit = header.length;
-        } else {
-          //if header value is false, set limit to bodyData length
-          limit = bodyData.length;
-        }
-         for(var i=0;i < limit;i++){
-          if(headValue){
-            //maping header with body data with header option is true.
-            result[header[i]]=bodyData[i];
-          } else {
-            //maping body data if header option is false.
-              result[i] = bodyData[i];
-          }
-         }
-         //pushing result object into array.
-         finalData.push(result);
-      };
+        var rawData = data.split('\n');
 
-      var rawData = data.split('\n');
-      //checking for limit options.
-      if(limit){
-        limit = limit+1;
-      } else {
-        limit = rawData.length-1;
-      }
-      for(var i =1 ;i<limit;i++){
-        //check headvale from options true||false
-        if(headValue)
-        {
-        // extract headers from entire data.
-        var header = getHeaders(data);
+        if(limit){
+          limit = limit+1;
         } else {
-          //if header is false send balnk array.
-        header = [];
+          limit = rawData.length-1;
         }
-        //create a object maping heades to data
-        var formatedData= combineData(header,rawData[i]);
-      };
-      console.log(finalData);
+
+        for(var i =1 ;i<limit;i++){
+          //check headvale from options true||false
+          if(headValue)
+          {
+          // extract headers from entire data.
+          var header = internals.getHeaders(data,delimiter);
+          } else {
+            //if header is false send balnk array.
+          header = [];
+          }
+          //create a object maping heades to data
+          var formatedData= internals.combineData(header,rawData[i],delimiter,headValue,finalData);
+        };
+        console.log(finalData);
     });
-  };
+};
+
+exports. csvToJson = function(path,options){
+      internals.parseData(path,options);
+};
